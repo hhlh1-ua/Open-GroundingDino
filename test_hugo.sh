7140c85c93f4fce075b32e94b4e34183e688e739
Hugo Hernández López <98743483+hhlh1-ua@users.noreply.github.com>
2025-01-27 23:08:35 +0100

    Update test_hugo.sh

--- test_hugo.sh
+++ test_hugo.sh
@@ -1,1 +1,13 @@
+GPU_NUM=$1
+CFG=$2
+DATASETS=$3
+OUTPUT_DIR=$4
 
+
+python  --nproc_per_node=${GPU_NUM} main.py \
+        --output_dir ${OUTPUT_DIR} \
+        --eval \
+        -config_file ${CFG} \
+        --datasets ${DATASETS}  \
+        --pretrain_model_path /path/to/groundingdino_swint_ogc.pth \
+        --options text_encoder_type=/path/to/bert-base-uncased
