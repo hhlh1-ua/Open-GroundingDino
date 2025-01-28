3df148c11aa302a8cd1cfe3827bccd92f4adf250
Hugo Hernández López <98743483+hhlh1-ua@users.noreply.github.com>
2025-01-28 18:37:07 +0100

    Update test_hugo.sh

--- test_hugo.sh
+++ test_hugo.sh
@@ -1,13 +1,30 @@
+# GPU_NUM=$1
+# CFG=$2
+# DATASETS=$3
+# OUTPUT_DIR=$4
+
+
+# python3 -m torch.distributed.launch  --nproc_per_node=${GPU_NUM} main.py \
+#         --output_dir ${OUTPUT_DIR} \
+#         --eval \
+#         --config_file ${CFG} \
+#         --datasets ${DATASETS}  \
+#         --pretrain_model_path /workspace/GroundingDINO/modelos/groundingdino_swint_ogc.pth \
+#         --options text_encoder_type=/workspace/GroundingDINO/modelos/BERT
+
 GPU_NUM=$1
 CFG=$2
 DATASETS=$3
 OUTPUT_DIR=$4
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
+PORT=${PORT:-29500}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 
-
-python3 -m torch.distributed.launch  --nproc_per_node=${GPU_NUM} main.py \
+python -m torch.distributed.launch  --nproc_per_node=${GPU_NUM} main.py \
         --output_dir ${OUTPUT_DIR} \
         --eval \
-        --config_file ${CFG} \
+        -c ${CFG} \
         --datasets ${DATASETS}  \
-        --pretrain_model_path /workspace/GroundingDINO/modelos/groundingdino_swint_ogc.pth \
-        --options text_encoder_type=/workspace/GroundingDINO/modelos/BERT
+        --pretrain_model_path /path/to/groundingdino_swint_ogc.pth \
+        --options text_encoder_type=/path/to/bert-base-uncased
