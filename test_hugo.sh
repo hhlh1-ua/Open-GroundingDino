3d3a0d5f0a13f1bab8a355918cf78c1884bdf6c5
Hugo Hernández López <98743483+hhlh1-ua@users.noreply.github.com>
2025-01-28 18:26:26 +0100

    Update test_hugo.sh

--- test_hugo.sh
+++ test_hugo.sh
@@ -7,7 +7,7 @@ OUTPUT_DIR=$4
 python3 -m torch.distributed.launch  --nproc_per_node=${GPU_NUM} main.py \
         --output_dir ${OUTPUT_DIR} \
         --eval \
-        -config_file ${CFG} \
+        --config_file ${CFG} \
         --datasets ${DATASETS}  \
         --pretrain_model_path /workspace/GroundingDINO/modelos/groundingdino_swint_ogc.pth \
         --options text_encoder_type=/workspace/GroundingDINO/modelos/BERT
