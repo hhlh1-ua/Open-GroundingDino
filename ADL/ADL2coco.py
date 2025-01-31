e0ccc7e45b7072f4a46f3ceb148d81ff9f73aacc
Hugo Hernández López <value>
2025-01-31 13:50:07 +0100

    Ya exporta anotaciones en formarto coco

--- ADL/ADL2coco.py
+++ ADL/ADL2coco.py
@@ -1,5 +1,5 @@
 import os
-
+import json, glob
 HEIGTH = 960
 WIDTH = 1280
 
@@ -15,62 +15,81 @@ annotation_id_counter = 1
 
 def createCOCO():
     global category_id_counter, image_id_counter, annotation_id_counter
+    errores=[]
     
-    for i in range(1, 7):
+    for i in range(1, 2):
         filename = f'object_annot_P_0{i}.txt' if i < 10 else f'object_annot_P_{i}.txt'
         filedir = os.path.join('annotations', 'object_annotation', filename)
-        
-        with open(filedir, "r") as file:
+        annotated_frames=f'object_annot_P_0{i}_annotated_frames.txt' if i < 10 else f'object_annot_P_{i}_annotated_frames.txt'
+        filedir_annotated_frames = os.path.join('annotations', 'object_annotation', annotated_frames)
+        with open(filedir_annotated_frames, "r") as file:
             for line in file:
                 line_vals = line.split()
-                category_name = line_vals[-1]  # Última palabra de la línea (categoría)
-                image_name = line_vals[5][2:]  # Nombre de la imagen
+                image_name = line_vals[0][2:]  # Nombre de la imagen
 
                 # Verificar si la imagen ya existe
                 if image_name not in images_dict:
-                    image_id_counter += 1
                     images_dict[image_name] = image_id_counter
                     images.append({
                         "id": image_id_counter,
                         "width": WIDTH,
                         "height": HEIGTH,
-                        "file_name": image_name
+                        "file_name": image_name+'.jpg'
                     })
+                    image_id_counter += 1
 
-                # Verificar si la categoría ya existe
-                if category_name not in category_names:
-                    category_names[category_name] = category_id_counter
-                    categories.append({
-                        "id": category_id_counter,
-                        "name": category_name,
-                        "supercategory": "none"
-                    })
-                    category_id_counter += 1
+        with open(filedir, "r") as file:
+            for line in file:
+                line_vals = line.split()
+                category_name = line_vals[-1]  # Última palabra de la línea (categoría)
+                image_name = line_vals[5][2:]  # Nombre de la imagen
+                if image_name in images_dict:
+                    # Verificar si la categoría ya existe
+                    if category_name not in category_names:
+                        category_names[category_name] = category_id_counter
+                        categories.append({
+                            "id": category_id_counter,
+                            "name": category_name,
+                            "supercategory": "none"
+                        })
+                        category_id_counter += 1
 
-                # Coordenadas de la caja delimitadora (bbox)
-                x1 = int(line_vals[1])
-                y1 = int(line_vals[2])
-                x2 = int(line_vals[3])
-                y2 = int(line_vals[4])
-                width = x2 - x1
-                height = y2 - y1
-                bbox = [x1, y1, width, height]
+                    # Coordenadas de la caja delimitadora (bbox)
+                    x1 = int(line_vals[1])
+                    y1 = int(line_vals[2])
+                    x2 = int(line_vals[3])
+                    y2 = int(line_vals[4])
+                    width = x2 - x1
+                    height = y2 - y1
+                    bbox = [x1, y1, width, height]
 
-                # Crear la anotación
-                annotation = {
-                    "id": annotation_id_counter,
-                    "image_id": images_dict[image_name],
-                    "category_id": category_names[category_name],
-                    "bbox": bbox,
-                    "area": width * height,
-                    "iscrowd": 0
-                }
-                annotations.append(annotation)
-                annotation_id_counter += 1
+                    # Crear la anotación
+                    annotation = {
+                        "id": annotation_id_counter,
+                        "image_id": images_dict[image_name],
+                        "category_id": category_names[category_name],
+                        "bbox": bbox,
+                        "area": width * height,
+                        "iscrowd": 0
+                    }
+                    annotations.append(annotation)
+                    annotation_id_counter += 1
+                else:
+                    error={
+                        "video":f'P{i}',
+                        "frame": image_name
+                    }
+                    errores.append(error)
+    coco={
+        "images": images,
+        "annotations": annotations,
+        "categories": categories
+    }
+    outdir='prueba.json'
+    with open(outdir, 'w') as out_file:
+        json.dump(coco, out_file, indent=4)
+    #print(errores)
 
-    # print("Categorías:", categories)
-    # print("Imágenes:", images)
-    # print("Anotaciones:", annotations)
 
 def main(args=None):
     createCOCO()
