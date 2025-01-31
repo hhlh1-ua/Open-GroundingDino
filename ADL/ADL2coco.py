69e24328bd56d2a1a7d36f0e41c5f56106d7fd77
Hugo Hernández López <value>
2025-01-31 15:50:42 +0100

    Casi conseguido el cambio a ADL

--- ADL/ADL2coco.py
+++ ADL/ADL2coco.py
@@ -5,18 +5,28 @@ import argparse
 HEIGTH = 960
 WIDTH = 1280
 
-categories = []  # Lista de categorías con IDs
-images = []  # Lista de imágenes con IDs
-annotations = []  # Lista de anotaciones
-category_names = {}  # Diccionario para asignar ID a categorías
-images_dict = {}  # Diccionario para asignar ID a imágenes
 
-image_id_counter = 0
-category_id_counter = 1
-annotation_id_counter = 1
+
+def chnage_cfg(test,categories):
+    if(test):
+        file="cfg_ADL_test.py"
+    else:
+        file="cfg_ADL_train.py"
+    path=os.path.join('config',file)
+    label_list_content = f'label_list = {json.dumps(list(categories.keys()))}\n'
+    print(label_list_content)
+
 
 def createCOCO(outfile,start_range, fin_range):
-    global category_id_counter, image_id_counter, annotation_id_counter
+    categories = []  # Lista de categorías con IDs
+    images = []  # Lista de imágenes con IDs
+    annotations = []  # Lista de anotaciones
+    category_names = {}  # Diccionario para asignar ID a categorías
+    images_dict = {}  # Diccionario para asignar ID a imágenes
+
+    image_id_counter = 0
+    category_id_counter = 1
+    annotation_id_counter = 1
     errores=[]
     
     for i in range(start_range, fin_range):
@@ -24,7 +34,8 @@ def createCOCO(outfile,start_range, fin_range):
         filedir = os.path.join('annotations', 'object_annotation', filename)
         annotated_frames=f'object_annot_P_0{i}_annotated_frames.txt' if i < 10 else f'object_annot_P_{i}_annotated_frames.txt'
         filedir_annotated_frames = os.path.join('annotations', 'object_annotation', annotated_frames)
-        prefix=f'P_0{i}'if i < 10 else'P_{i}'
+        prefix=f'P_0{i}'if i < 10 else f'P_{i}'
+        print(f'getting annotations for frames in video {prefix}')
         with open(filedir_annotated_frames, "r") as file:
             for line in file:
                 line_vals = line.split()
@@ -92,19 +103,23 @@ def createCOCO(outfile,start_range, fin_range):
     outdir= os.path.join('annotations','coco_format',outfile)
     with open(outdir, 'w') as out_file:
         json.dump(coco, out_file, indent=4)
-    #print(errores)
+    return category_names
 
 
 def main(args):
     if(args.test):
-        createCOCO(outfile='annotations_test.json',start_range=11,fin_range=17)
+        categories=createCOCO(outfile='annotations_test.json',start_range=11,fin_range=17)
+        chnage_cfg(True,categories)
     if(args.validation):
         createCOCO(outfile='annotations_val.json',start_range=17,fin_range=21)
     if(args.train):
-       createCOCO(outfile='annotations_train.json',start_range=1,fin_range=11)
+       categories=createCOCO(outfile='annotations_train.json',start_range=1,fin_range=11)
+       chnage_cfg(False,categories)
     if(args.all):
-        createCOCO(outfile='annotations_train.json',start_range=1,fin_range=11)
-        createCOCO(outfile='annotations_test.json',start_range=11,fin_range=17)
+        categories=createCOCO(outfile='annotations_train.json',start_range=1,fin_range=11)
+        chnage_cfg(False,categories)
+        categories=createCOCO(outfile='annotations_test.json',start_range=11,fin_range=17)
+        chnage_cfg(True,categories)
         createCOCO(outfile='annotations_val.json',start_range=17,fin_range=21)
 
 
