6bde513022da47582e67c8bea5cc225409fcf2b8
Hugo Hernández López <value>
2025-01-31 01:01:52 +0100

    crea las images

--- ADL/ADL2coco.py
+++ ADL/ADL2coco.py
@@ -1,15 +1,19 @@
 import os
 import pandas as pd
 
-WIDTH = 960
-HEIGTH = 1280
+HEIGTH = 960
+WIDTH = 1280
 
 categories = []  # Lista de categorías
+images = []
 category_names = set()  # Conjunto para verificar nombres únicos
+images_names = set()
+image_id_counter = 0
 category_id_counter = 1  # Contador de ID autoincremental
 
 def createCOCO():
     global category_id_counter  # Hacer el contador global para que persista
+    global image_id_counter
     for i in range(1,7):
         if(i<10):
             filename = f'object_annot_P_0{i}.txt'
@@ -20,6 +24,18 @@ def createCOCO():
             for line in file:
                 line_vals = line.split()
                 category_name = line_vals[-1]  # Última palabra de la línea
+                image_name= line_vals [5][2:]
+
+                if image_name not in images_names:
+                    image={
+                        "id": image_id_counter,
+                        "width": WIDTH,
+                        "heigth": HEIGTH,
+                        "file_name":image_name
+                    }
+                    images.append(image)
+                    images_names.add(image_name)
+                    image_id_counter+=1
                 
                 if category_name not in category_names:
                     category = {
@@ -33,6 +49,7 @@ def createCOCO():
                     category_id_counter += 1  # Incrementar el contador para la siguiente categoría
     
     print(categories)  # Mostrar las categorías únicas con ID autoincremental
+    print(images)
 
 def main(args=None):
     createCOCO()
