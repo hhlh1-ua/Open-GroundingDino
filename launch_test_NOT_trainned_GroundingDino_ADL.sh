2e05789a448228c0692889ce93d7ffda03c75271
hhlh1-ua <hhlh1@alu.ua.es>
2025-02-10 21:09:51 +0100

    Inference sobre el GR fine-tunned sobre COCO hecha

--- launch_test_NOT_trainned_GroundingDino_ADL.sh
+++ launch_test_NOT_trainned_GroundingDino_ADL.sh
@@ -2,9 +2,12 @@ GPU_NUM=1
 CFG="/workspace/GroundingDINO/Open-GroundingDino/config/cfg_ADL_test.py"
 DATASETS="/workspace/GroundingDINO/Open-GroundingDino/config/dataset_test_ADL.json"
 #OUTPUT_DIR="/workspace/GroundingDINO/Open-GroundingDino/resultados/pre_finetunning"
-OUTPUT_DIR="/workspace/GroundingDINO/Open-GroundingDino/resultados/pre_finetunning/Version FineTunning en COCO"
-# PATH_TO_MODEL= "/workspace/GroundingDINO/modelos/groundingdino_swint_ogc.pth"
-PATH_TO_MODEL= "/workspace/GroundingDINO/modelos/gdinot-coco-ft.pth"
+# OUTPUT_DIR="/workspace/GroundingDINO/Open-GroundingDino/resultados/pre_finetunning/Version_FineTunneada_en_COCO"
+OUTPUT_DIR="/workspace/GroundingDINO/Open-GroundingDino/resultados/pre_finetunning/Version_entrenada_con_1M"
+# /workspace/GroundingDINO/modelos/groundingdino_swint_ogc.pth
+# /workspace/GroundingDINO/modelos/gdinot-coco-ft.pth
+# /workspace/GroundingDINO/modelos/gdinot-1.8m-odvg.pth
+
 
 NNODES=${NNODES:-1}
 NODE_RANK=${NODE_RANK:-0}
@@ -16,6 +19,6 @@ python3 -m torch.distributed.launch  --nproc_per_node=${GPU_NUM} main.py \
         --eval \
         -c ${CFG} \
         --datasets ${DATASETS}  \
-        --pretrain_model_path ${PATH_TO_MODEL} \
+        --pretrain_model_path /workspace/GroundingDINO/modelos/gdinot-1.8m-odvg.pth \
         --options text_encoder_type=/workspace/GroundingDINO/modelos/BERT \
         --save_results
