80f03f6d9fab752cfabfb20dcb5210247775a0ef
hhlh1-ua <hhlh1@alu.ua.es>
2025-02-10 19:54:41 +0100

    Creadas carpetas para diferentes modelos de grounding DINO

--- launch_test_NOT_trainned_GroundingDino_ADL.sh
+++ launch_test_NOT_trainned_GroundingDino_ADL.sh
@@ -1,7 +1,11 @@
 GPU_NUM=1
 CFG="/workspace/GroundingDINO/Open-GroundingDino/config/cfg_ADL_test.py"
 DATASETS="/workspace/GroundingDINO/Open-GroundingDino/config/dataset_test_ADL.json"
-OUTPUT_DIR="/workspace/GroundingDINO/Open-GroundingDino/resultados/pre_finetunning"
+#OUTPUT_DIR="/workspace/GroundingDINO/Open-GroundingDino/resultados/pre_finetunning"
+OUTPUT_DIR="/workspace/GroundingDINO/Open-GroundingDino/resultados/pre_finetunning/Version FineTunning en COCO"
+# PATH_TO_MODEL= "/workspace/GroundingDINO/modelos/groundingdino_swint_ogc.pth"
+PATH_TO_MODEL= "/workspace/GroundingDINO/modelos/gdinot-coco-ft.pth"
+
 NNODES=${NNODES:-1}
 NODE_RANK=${NODE_RANK:-0}
 PORT=${PORT:-29500}
@@ -12,6 +16,6 @@ python3 -m torch.distributed.launch  --nproc_per_node=${GPU_NUM} main.py \
         --eval \
         -c ${CFG} \
         --datasets ${DATASETS}  \
-        --pretrain_model_path /workspace/GroundingDINO/modelos/groundingdino_swint_ogc.pth \
+        --pretrain_model_path ${PATH_TO_MODEL} \
         --options text_encoder_type=/workspace/GroundingDINO/modelos/BERT \
         --save_results
